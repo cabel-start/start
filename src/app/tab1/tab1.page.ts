cb24b0c4b3f70dff87489e650fa1ca524730be32
Mario <mario.pavlovic@dings.solutions>
2019-12-29 21:16:49 +0100

    Integrate google maps

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +17,46 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  map: GoogleMap;
+
   constructor() {}
+
+  ngAfterViewInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDklM3s3igv1VKZOQqJEuPD4xgvbZu-KTw',
+      'API_KEY_FOR_BROWSER_DEBUG': ''
+    })
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
 
 }
